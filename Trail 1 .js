658191872d52da8337c8b5b7da648d1ef8a24567
Nir Adi <NirAdi72@gmail.com>
2024-11-14 14:07:43 +0200

    my first commit

---
+++ Trail 1 .js
@@ -0,0 +1,25 @@
+// My first ever Javascript file
+console.log("hello world");
+//how did i do?
+let myname = " Hana Regev"; // the data the varible has
+console.log("my name is" + myname);
+//Third practice
+let mynumber = "85457";
+console.log("my favorite number is ", mynumber);
+let student = {
+  name: "Haim",
+  age: 32,
+};
+console.log(student.age, student.name);
+//constant elements
+const students = ["John", "Haim", "Reginal"];
+console.log(students[0], students[1], students[2]);
+//first function>?
+let number = 58;
+function average(A, B, C) {
+  let result = (A + B + C) / 3;
+  return result;
+}
+console.log("THIS IS THE RESULT", average(10, 20, 15));
+//second one
+function minimun(Z, X, E, V, N) {}
