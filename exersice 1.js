ff82262b9536572e3e95fc937004d81cfcaa09c6
Nir Adi <NirAdi72@gmail.com>
2024-11-14 14:45:50 +0200

    home exersice 1

---
+++ exersice 1.js
@@ -0,0 +1,9 @@
+const firstName = 'Nir'
+const middleName = 'Nissim'
+const lastName = 'Adi'
+console.log(firstName,middleName,lastName)
+let age = 30
+age = 29 
+console.log(age)
+const date = '18.10.1994'
+console.log(date)
