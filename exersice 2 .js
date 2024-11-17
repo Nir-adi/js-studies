89a89f9c9962487d01bd3ad75ea58d489ac634aa
Nir Adi <NirAdi72@gmail.com>
2024-11-17 10:27:40 +0200

    תרגיל 2

---
+++ exersice 2 .js
@@ -0,0 +1,14 @@
+//
+let length = 10 
+let width = 20
+let surface = length*width
+
+
+let length2 = 15
+let width2 = 30 
+let surface2 = length2*width2
+console.log(surface,surface2)
+
+let result = surface === surface2
+
+console.log(result)
